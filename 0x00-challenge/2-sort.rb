ee4f207b69ddc4f1a81d5f6f9d94330f2ef9f298
PaulaMaina <kabura.mainap@gmail.com>
2024-03-22 15:23:40 +0300

    Fix my code

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -16,16 +16,15 @@ ARGV.each do |arg|
     is_inserted = false
     i = 0
     l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
+    while i < l do
+        if result[i] > i_arg
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
+        i += 1
     end
     result << i_arg if !is_inserted
 end
 
-puts result
+puts result.reverse
